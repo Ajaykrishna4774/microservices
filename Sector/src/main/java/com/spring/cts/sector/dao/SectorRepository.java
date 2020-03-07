acc28ff3a605545d9682dbd879c222e418708d60
AJAY <43701958+Ajaykrishna4774@users.noreply.github.com>
2020-03-07 17:51:10 +0530

    Add files via upload

---
+++ Sector/src/main/java/com/spring/cts/sector/dao/SectorRepository.java
@@ -0,0 +1,11 @@
+package com.spring.cts.sector.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.spring.cts.sector.pojo.Sector;
+
+
+
+public interface SectorRepository  extends CrudRepository<Sector,Integer> {
+
+}
